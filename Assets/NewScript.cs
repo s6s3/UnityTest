b0b8c8b12af0a01c85758d38c46136f328ebf227
s6s3 <z.zz.zzz.last.night@gmail.com>
2016-08-16 16:02:44 +0100

    init

---
+++ Assets/NewScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewScript : MonoBehaviour {
+
+	private float speed = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody> ().AddForce ((transform.forward + transform.right) * speed, ForceMode.VelocityChange);
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
